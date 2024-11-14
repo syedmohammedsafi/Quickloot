77c743cd94fe9867bb2274ad351096c6191f83ea
Syed Mohammed Safi S A <safi22052004@gmail.com>
2024-11-14 21:53:35 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: '.',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+});
